f842d4b7f0651ca94736c709f051aebcdf53f05b
Usman Hameed <85105769+Usman-Gray-Hat@users.noreply.github.com>
2021-11-30 20:53:20 +0500

    Variables

---
+++ Php/Variables.php
@@ -0,0 +1,20 @@
+<?php
+$name = "Usman";
+$Age = 23;
+$Email = "usman@gmail.com";
+
+echo "<u><h1>"."Student Information:"."<h1/>";
+echo "<br>";
+
+
+echo "Student name is ".$name;
+echo "<br>";
+
+echo "Student Age is ".$Age;
+echo "<br>";
+
+echo "Student email is ".$Email;
+echo "<br>";
+
+
+?>
