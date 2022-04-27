18fa63707ae467a33d534fa4444909de5b5da14c
Usman Hameed <85105769+Usman-Gray-Hat@users.noreply.github.com>
2022-04-27 22:24:47 +0500

    Passing Data From Controller To View

---
+++ Laravel/PassingDataFromControllerToView/compactFunction/Index.blade.php
@@ -0,0 +1,8 @@
+
+     
+     <h1> My name is {{ $name }} </h1>
+     <h1> My age is {{ $age }} </h1>
+     <h1> My gender is {{ $gender }} </h1>
+
+
+
---
+++ Laravel/PassingDataFromControllerToView/nameArray/Index.blade.php
@@ -0,0 +1,31 @@
+
+     <h1> For Fuits </h1>
+<ol>
+
+    <li> {{ $Fruits[0] }} </li>
+    <li> {{ $Fruits[1] }} </li>
+    <li> {{ $Fruits[2] }} </li>
+
+</ol>
+
+<br>
+
+     <h1> For Sports </h1>
+<ol>
+
+    <li> {{ $Sports[0] }} </li>
+    <li> {{ $Sports[1] }} </li>
+    <li> {{ $Sports[2] }} </li>
+
+</ol>
+
+<br>
+
+<h1> For Colors </h1>
+<ol>
+
+    <li> {{ $Colors[0] }} </li>
+    <li> {{ $Colors[1] }} </li>
+    <li> {{ $Colors[2] }} </li>
+
+</ol>
