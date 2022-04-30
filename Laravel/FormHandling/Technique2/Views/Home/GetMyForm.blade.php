5ae7ec61ac8c65a3217d44a5f32e3d53e222a938
Usman Hameed <85105769+Usman-Gray-Hat@users.noreply.github.com>
2022-04-30 21:06:40 +0500

    Delete Laravel/FormHandling directory

--- Laravel/FormHandling/Technique2/Views/Home/GetMyForm.blade.php
+++
@@ -1,6 +0,0 @@
-
-<h1> Name is: {{ $FormData['name'] }} </h1>
-<h1> Age is: {{ $FormData['age'] }} </h1>
-<h1> Gender is: {{ $FormData['gender'] }} </h1>
-<h1> Email is: {{ $FormData['email'] }} </h1>
-<h1> Country is: {{ $FormData['country'] }} </h1>
--- Laravel/FormHandling/Techniue1/Views/Home/Getdata.blade.php
+++
@@ -1,4 +0,0 @@
-
-<h1> Your name is: {{ $FormData['name'] }} </h1>
-<h1> Your email is: {{ $FormData['email'] }} </h1>
-<h1> Your age is: {{ $FormData['age'] }} </h1>
